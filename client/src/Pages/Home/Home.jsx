75feb1eb70638abce05e4e7ab3af4c53a8dd4295
Teslim <steslimolayiwola@gmail.com>
2024-03-26 22:30:01 +0100

    addition of Home sectionA

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -9,7 +9,12 @@ const Home = () => {
    <Header />
     <div>
       <div>
-        
+        <img className='relative' src={bgimg} alt="bgimg" />
+        <div className='absolute rounded'>
+          <p>Help Restore Dilapidated Mosques</p>
+          <h1>The Prophet (PBUH) said: “Whoever builds a masjid for the sake of Allah, Allah will build for him a house in Paradise.”</h1>
+
+        </div>
       </div>
 
 
