cca9e16648a8de4a6a04e88269accf40cb37e3e2
Teslim <steslimolayiwola@gmail.com>
2024-04-01 01:54:22 +0100

    addition of Registeration page

--- client/src/Pages/Register/Register.jsx
+++ client/src/Pages/Register/Register.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import Header from '../../Components/Header/Header' 
 import Footer from '../../Components/Footer/Footer'
+import logo from '../../Components/../assets/logo.png'
 
 const Register = () => {
   return (
     <div>
-      <Header />
+      <div>
+        <div className='items-start'>
+          <img className='w-[94px] h-[80px] ml-[10%] mt-10' src={logo} alt="" />
+        </div>
+        <div className='rounded-lg w-[462px] h-[1105px] justify-center ml-[30%] mb-10 top-32 left-60 items-center gap-6 p-2'>
+        <div className='text-4xl w-[82%] items-center font-medium'>
+          <p>Register An Account</p>
+        </div>
+        </div>
 
-
-      <Footer />
+      </div>
     </div>
   )
 }
