33ae898dce66cf41a3423ca4f54834e9636da0a8
Teslim <steslimolayiwola@gmail.com>
2024-04-05 22:55:52 +0100

    addition of section/about page

--- client/src/Pages/About/About.jsx
+++ client/src/Pages/About/About.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
-import Header from '../../Components/Header/Header'
-import Footer from '../../Components/Footer'
+import React from "react";
+import { Link } from "react-router-dom";
+import Header from "../../Components/Header/Header";
+import Footer from "../../Components/Footer";
+import msjwind from "../../Components/../assets/msjwind.png";
 
 const About = () => {
   return (
     <div>
       <Header />
-
+      <div className="pt-20 flex items-center justify-evenly pl-4 w-[87%] gap-2 h-[31%] py-3">
+        <div className="w-[55%]">
+          <h3 className="text-5xl font-semibold">About Ibni Masjid Lillah </h3>
+          <h1 className="text-2xl font-medium leading-9 text-[#001712]">
+            Ibni Masjid Lillah is a non-profit Islamic organization dedicated to
+            restoring and renovating mosques in Nigeria. Through transparent
+            initiatives and dedicated efforts, we aim to connect donors with
+            mosque restoration projects, creating a positive impact on
+            worshipers and communities, ensuring mosques remain centers of
+            faith, unity, education, and development.
+          </h1>
+        </div>
+        <div className="w-[43%]">
+          <img src={msjwind} alt="" />
+        </div>
+      </div>
 
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
