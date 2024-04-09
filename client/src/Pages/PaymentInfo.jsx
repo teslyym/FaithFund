93b854df7f0278364beba9ddb1e86218e8745e70
Teslim <steslimolayiwola@gmail.com>
2024-04-09 22:05:20 +0100

    addition of Payment Info page

---
+++ client/src/Pages/PaymentInfo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PaymentInfo = () => {
+  return (
+    <div>
+      <div className="w-[428px]- rounded-lg">
+        <div className="w-[380px]"></div>
+      </div>
+    </div>
+  );
+};
+
+export default PaymentInfo;
