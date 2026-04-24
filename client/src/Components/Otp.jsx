02dc310e71a0f911b475a1aa564aa4e3f1ff0af9
Teslim <steslimolayiwola@gmail.com>
2026-04-24 20:36:43 +0100

    fixed Otp and PaymentInfo components for improved layout and styling

--- client/src/Components/Otp.jsx
+++ client/src/Components/Otp.jsx
@@ -5,38 +5,45 @@ import CustomButtons from "./buttons/CustomButtons";
 const Otp = ({ setDonationSteps }) => {
   return (
     <div>
-      <div className="w-full h-screen flex flex-col justify-center pt-5 pr-6 pb-5 pl-6 top-[220px] items-center flex-wrap">
-        <h1 className="">Input OTP</h1>
-        <div className="flex gap-2 pt-3 justify-center">
-          <div className="w-[50px] h-[50px] rounded border border-[#9DA39F]">
+      <div className="w-full min-h-screen flex flex-col justify-center items-center px-4 sm:px-6">
+        <h1 className="text-lg sm:text-xl md:text-2xl font-semibold text-center">
+          Input OTP
+        </h1>
+
+        <div className="flex gap-2 sm:gap-3 pt-4 justify-center flex-wrap">
+          <div className="w-11 h-11 sm:w-[50px] sm:h-[50px] rounded border border-[#9DA39F]">
             <input
               type="text"
               placeholder=""
-              className="flex  px-2 py-3 items-center  w-full"
+              className="w-full h-full px-2 text-center outline-none"
             />
           </div>
-          <div className="w-[50px] h-[50px] rounded border border-[#9DA39F]">
+
+          <div className="w-11 h-11 sm:w-[50px] sm:h-[50px] rounded border border-[#9DA39F]">
             <input
               type="text"
               placeholder=""
-              className="flex  px-2 py-3 items-center  w-full"
+              className="w-full h-full px-2 text-center outline-none"
             />
           </div>
-          <div className="w-[50px] h-[50px] rounded border border-[#9DA39F]">
+
+          <div className="w-11 h-11 sm:w-[50px] sm:h-[50px] rounded border border-[#9DA39F]">
             <input
               type="text"
               placeholder=""
-              className="flex  px-2 py-3 items-center  w-full"
+              className="w-full h-full px-2 text-center outline-none"
             />
           </div>
-          <div className="w-[50px] h-[50px] rounded border border-[#9DA39F]">
+
+          <div className="w-11 h-11 sm:w-[50px] sm:h-[50px] rounded border border-[#9DA39F]">
             <input
               type="number"
               placeholder=""
-              className="flex  px-2 py-3 items-center  w-full"
+              className="w-full h-full px-2 text-center outline-none"
             />
           </div>
         </div>
+
         <Link className="pt-6">
           <CustomButtons
             handleClick={() => setDonationSteps("confirmation")}
