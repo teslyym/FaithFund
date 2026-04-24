80820c32e5e0d48b883813668a9dbd78ae000037
Teslim <steslimolayiwola@gmail.com>
2026-04-24 21:39:25 +0100

    Refactor CORS middleware configuration for improved security and flexibility

--- server/index.js
+++ server/index.js
@@ -1,18 +1,52 @@
+// import express from "express";
+// import cors from "cors";
+// import connectdb from "./utils/db.js";
+// import dotenv from "dotenv";
+// import userRoutes from "./routes/userRoutes.js";
+// import mosqueRoutes from "./routes/mosqueRoutes.js";
+// // import Users from "./Models/userModel";
+// const app = express();
+// dotenv.config();
+// const port = process.env.PORT || 5001;
+// connectdb();
+
+// app.use(cors());
+
+// app.use(express.json());
+// app.get("/", (req, res) => {
+//   res.send("api works");
+// });
+
+// app.use("/api/user", userRoutes);
+// app.use("/api/mosque", mosqueRoutes);
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}!`);
+// });
 import express from "express";
 import cors from "cors";
 import connectdb from "./utils/db.js";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import mosqueRoutes from "./routes/mosqueRoutes.js";
-// import Users from "./Models/userModel";
+
 const app = express();
 dotenv.config();
+
 const port = process.env.PORT || 5001;
+
 connectdb();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://faith-fund.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("api works");
 });
